337e8947c287c1271daef5de3b38635828511098
Prxyankaz <137303413+Prxyankaz@users.noreply.github.com>
2025-03-28 00:14:19 +0530

    G

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3756805804"
+    serviceWorkerVersion: "2346210260"
   }
 });
